65ac9b29551e7b4a03622d0dc1c460bfa1a68642
diaodiaosvip <35662637+diaodiaosvip@users.noreply.github.com>
2021-02-25 14:50:20 +0800

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,104 @@
+<?php return array (
+  'site_name' => 'OneIndex',
+  'password' => 'oneindex',
+  'style' => 'nexmoe',
+  'onedrive_root' => '',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'vvlOMH44meojFHDL406$[]}',
+  'client_id' => '4a36bc13-ebbf-43a5-9402-3fc2a5b88262',
+  'redirect_uri' => 'https://diaodiaosvip.github.io/oneindex.github.io',
+  'refresh_token' => '0.AAAAk8JdVgs020GFg4TYf6uRThO8Nkq_66VDlAI_wqW4gmJWAHw.AgABAAAAAAD--DLA3VO7QrddgJg7WevrAgDs_wQA9P-GPRhEqhT9E4H_eQTApsVLF146fLxFGDNL3qfLiewBGcX1igUzhYL6F3pLgbq9LlzMcCTOgJXiR9X-e_hQOJahD5oG0i6Ksge_R-x6i9pX9qcQ0P0VF7BKbCnrY0kRv2Yk9YYIVIWKKFJbEAe9S9vowjyhvPsxnVeil7l8Z8GTXbFCCzN83s0Lzep-qJ7TpeNTEkt5fzA53LHuDSnjr_R-Z0dEuh1TUqncxQrwZ0jUCMtLp3UNoPwIfw5C-HeL_EWfCNrPu7PQYCDbcV9g1zKyRRDqpgZ-z-asVY54UCL-wDmklnN1u2f02mbpGIRBMFm4b4XGaB6rI5GS8cXKCI170LHCh70VcWB1P8YS67lJx146_DPvbIdHeJbJupNd3QqbtgpjGz01pvXVa5kZYnJxm7fpskIgaND9x3MxwaqLVgNnI-mrkRNcLWvBpx-_EeijQKMpBbHQNlive14DB9bFCdRmCJywuyYjSv3WynUHVqX9m79FhsVbb0QamsFQJeKf2v9I-hq91p_fzDiUmi8LY95r97xwv47dLWjvO7Lkp_GahJYPTJEZNjidSeHfYYubvVeyiDHMoNAuc7MxY_0DIJZiXkWY0M_DWKHLb2jWNBSv1SwY-mAqrfb4aN5fxmJ5d-c_nAj9FRmvASCBi_T-2WARquOMkXC7fokhPckgxK6rlwPAST7vwDEcotOWVs3FBhXAYXOJAPUvxPl6AfHclPw4ghEoF_R8FR_wOuE2F-RvoxXrJKmaAD21tcg-kljMPLhH56tDdbBlKP6AsojIh4aZ5pzTWtSJhhPZNIwa5U3ywRfe6CvZT8Gzcp_EM2-SAq9k4p6Nf0EkBAUpQjBLaN1O6wUQQgFQRLP_TH5Cxlz67Q2i1EHfSrUPk3_VEJ61tXRo8FAnSNhqlfw-hmT_AWLHl_VNLLuNDrZVw-S7GD6kvMHPYMEG4v-DBEpyfN1p_W3h0FW4edwXVb2AaCVm6vNyD29I7Ry6aEwTdiPvMNT72CmguEZ5OtdwmyrMA6eAkVHHKM6gMA6f00dhNHxR2mrH-DKD5MZUAbTgQfq3Z_Ptqw',
+);
